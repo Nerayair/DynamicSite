78a7733a3eb95d5b5fe1216e6d4d6713714a62ef
Yair Arieli <yair@joyned.co>
2025-03-18 22:35:19 +0200

    Second commit test.

--- Frontend/script.js
+++ Frontend/script.js
@@ -3,4 +3,5 @@ function storeInput() {
   const input = document.getElementById("promptInput").value;
   localStorage.setItem("promptInput", input);
   console.log("Input has been stored in local storage.");
+  console.log("Redirecting to /output...");
 }
