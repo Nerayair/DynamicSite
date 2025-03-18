28ceaa6160447df14e347c43fa8793269ab8b397
Yair Arieli <yair@joyned.co>
2025-03-18 22:15:45 +0200

    Front: initial imp.

---
+++ Frontend/script.js
@@ -0,0 +1,6 @@
+function storeInput() {
+  console.log("Storing input in local storage...");
+  const input = document.getElementById("promptInput").value;
+  localStorage.setItem("promptInput", input);
+  console.log("Input has been stored in local storage.");
+}
