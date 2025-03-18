855c0b602d01805e910dc289c251356c00863144
neriaad <71095212+neriaad@users.noreply.github.com>
2025-03-18 21:53:09 +0200

    Update get_gemini_response.py

--- Backend/get_gemini_response.py
+++ Backend/get_gemini_response.py
@@ -1,1 +1,13 @@
+import google.generativeai as genai
+import os
 
+GOOGLE_API_KEY='AIzaSyB2Hbwe_EcdHw6ctrUJBOlt00X_rgQogFM'
+
+genai.configure(api_key=GOOGLE_API_KEY)
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+response = model.generate_content("""how much is 1 + 1""")
+print(response.candidates)
+print(response.resolve())
+print(dir(response))
